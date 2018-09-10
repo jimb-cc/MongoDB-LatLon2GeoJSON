c8ccb4ace0748efc0e707ecfb404e9f97190780b
Jim Blackhurst <jim@jimb.cc>
2018-09-10 15:28:33 +0100

    No more of that struct nonsense, do it all in the bson.Document

--- main.go
+++ main.go
@@ -9,12 +9,70 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/mongodb/mongo-go-driver/bson"
 	"github.com/mongodb/mongo-go-driver/mongo"
 )
+
+func main() {
+	fmt.Println("-- Lat/Lon conversion to GeoJSON")
+	ctx := context.Background()
+
+	client, err := mongo.NewClient("mongodb://localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db := client.Database("AIS")
+
+	readDocs(ctx, db)
+}
+
+func readDocs(ctx context.Context, db *mongo.Database) error {
+
+	c, err := db.Collection("ais_10").Find(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("Couldn't find any data: %v", err)
+	}
+	defer c.Close(ctx)
+
+	for c.Next(ctx) {
+		doc := bson.NewDocument()
+		if err = c.Decode(doc); err != nil {
+			return fmt.Errorf("can't decode a doc: %v", err)
+		}
+
+		doc.Append(
+			bson.EC.SubDocumentFromElements("coordinates",
+				bson.EC.String("type", "Point"),
+				bson.EC.ArrayFromElements("coordinates", bson.VC.Decimal128(doc.Lookup("Latitude").Decimal128()), bson.VC.Decimal128(doc.Lookup("Longitude").Decimal128()))))
+
+		fmt.Printf("lat: %v -- lon: %v", doc.Lookup("Latitude").Decimal128(), doc.Lookup("Longitude").Decimal128())
+		fmt.Println("\n----------------------------\n")
+
+		spew.Dump(doc)
+		fmt.Println("----------------------------\n\n")
+
+	}
+
+	if err = c.Err(); err != nil {
+		return fmt.Errorf("all data couldn't be listed: %v", err)
+	}
+	return nil
+}
+
+/*
+
+"coordinates":{
+	"type":"Point",
+	"coordinates":[-118.21171,33.77161]
+	}
 
 type aisRecord struct {
 	//objectID       string `json:"id"`
@@ -37,65 +95,27 @@ type aisRecord struct {
 	AdditionalInfo string
 	MovementID     int64
 }
+*/
 
-func main() {
-	fmt.Println("-- Lat/Lon conversion to GeoJSON")
-	ctx := context.Background()
-
-	client, err := mongo.NewClient("mongodb://localhost")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = client.Connect(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	db := client.Database("test")
-
-	readDocs(ctx, db)
-}
-
-func readDocs(ctx context.Context, db *mongo.Database) error {
-	c, err := db.Collection("ais").Find(ctx, nil)
-	if err != nil {
-		return fmt.Errorf("Couldn't find any data: %v", err)
-	}
-	defer c.Close(ctx)
-
-	for c.Next(ctx) {
-		elem := bson.NewDocument()
-		if err = c.Decode(elem); err != nil {
-			return fmt.Errorf("can't decode a doc: %v", err)
-		}
-		s := aisRecord{
-			//objectID:       elem.Lookup("_id").StringValue(),
-			LRIMOShipNo:    elem.Lookup("LRIMOShipNo").StringValue(),
-			ShipName:       elem.Lookup("ShipName").StringValue(),
-			ShipType:       elem.Lookup("ShipType").StringValue(),
-			MMSI:           elem.Lookup("MMSI").Int32(),
-			CallSign:       elem.Lookup("CallSign").StringValue(),
-			Latitude:       elem.Lookup("Latitude").Double(),
-			Longitude:      elem.Lookup("Longitude").Double(),
-			Length:         elem.Lookup("Length").Int32(),
-			Draught:        elem.Lookup("Draught").Double(),
-			Beam:           elem.Lookup("Beam").Int32(),
-			Heading:        elem.Lookup("Heading").Double(),
-			Speed:          elem.Lookup("Speed").Double(),
-			Destination:    elem.Lookup("Destination").StringValue(),
-			ETA:            elem.Lookup("ETA").DateTime(),
-			MoveStatus:     elem.Lookup("MoveStatus").StringValue(),
-			MoveDateTime:   elem.Lookup("MovementDateTime").DateTime(),
-			AdditionalInfo: elem.Lookup("AdditionalInfo").StringValue(),
-			MovementID:     elem.Lookup("MovementID").Int64()}
-
-		spew.Printf("%#v", s)
-
-	}
-
-	if err = c.Err(); err != nil {
-		return fmt.Errorf("all data couldn't be listed: %v", err)
-	}
-	return nil
-}
+/*
+	s := aisRecord{
+		//objectID:       elem.Lookup("_id").StringValue(),
+		LRIMOShipNo:    elem.Lookup("LRIMOShipNo").StringValue(),
+		ShipName:       elem.Lookup("ShipName").StringValue(),
+		ShipType:       elem.Lookup("ShipType").StringValue(),
+		MMSI:           elem.Lookup("MMSI").Int32(),
+		CallSign:       elem.Lookup("CallSign").StringValue(),
+		Latitude:       elem.Lookup("Latitude").Double(),
+		Longitude:      elem.Lookup("Longitude").Double(),
+		Length:         elem.Lookup("Length").Int32(),
+		Draught:        elem.Lookup("Draught").Double(),
+		Beam:           elem.Lookup("Beam").Int32(),
+		Heading:        elem.Lookup("Heading").Double(),
+		Speed:          elem.Lookup("Speed").Double(),
+		Destination:    elem.Lookup("Destination").StringValue(),
+		ETA:            elem.Lookup("ETA").DateTime(),
+		MoveStatus:     elem.Lookup("MoveStatus").StringValue(),
+		MoveDateTime:   elem.Lookup("MovementDateTime").DateTime(),
+		AdditionalInfo: elem.Lookup("AdditionalInfo").StringValue(),
+		MovementID:     elem.Lookup("MovementID").Int64()}
+*/
